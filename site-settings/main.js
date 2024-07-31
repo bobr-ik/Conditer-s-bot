8d81332d0de44508cc98ab949d6a320d9402bf52
bobr-ik <aavramenko804@gmail.com>
2024-07-31 15:09:06 +0300

    Add files via upload

--- site-settings/main.js
+++ site-settings/main.js
@@ -1,6 +1,10 @@
 var tg = window.Telegram.WebApp;
 function cliick(num){
+    var num = {
+        '111': '123',
+        '123': '134'
+    }
     console.log('aaaaaaaaa');
-    tg.sendData(num);
+    tg.sendData(JSON.stringify(num));
     tg.close();
 }
